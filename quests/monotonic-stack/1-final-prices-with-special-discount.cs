edcb1293e91315befbc3174d0d327e1bd5cc57d2
Mahammad Ahmadov <dev.ahmadov.mahammad@gmail.com>
2026-06-09 14:38:53 +0400

    quests: add second solution to q1

--- quests/monotonic-stack/1-final-prices-with-special-discount.cs
+++ quests/monotonic-stack/1-final-prices-with-special-discount.cs
@@ -1,22 +1,40 @@
 // Q1. Final Prices With a Special Discount in a Shop
-public class Solution
+public class Solution 
 {
-    public int[] FinalPrices(int[] prices)
+    public int[] FinalPrices(int[] prices) 
     {
-        // Solution 1 :
+        // Solution 1 : 
+
+        // for(int i = 0; i < prices.Length; i++)
+        // {
+        //     int val = prices[i];
+
+        //     for(int j = i + 1; j < prices.Length; j++)
+        //     {
+        //         if(prices[j] <= prices[i])
+        //         {
+        //             prices[i] -= prices[j];
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // return prices;
+
+
+
+        // Solution 2 : 
+
+        Stack<int> stack = new Stack<int>();
 
         for(int i = 0; i < prices.Length; i++)
         {
-            int val = prices[i];
-
-            for(int j = i + 1; j < prices.Length; j++)
+            while(stack.Count != 0 && prices[stack.Peek()] >= prices[i])
             {
-                if(prices[j] <= prices[i])
-                {
-                    prices[i] = prices[i] - prices[j];
-                    break;
-                }
+                prices[stack.Pop()] -= prices[i];
             }
+
+            stack.Push(i);
         }
 
         return prices;
