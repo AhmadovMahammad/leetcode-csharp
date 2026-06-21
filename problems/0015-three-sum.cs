591bddb0e40aeb1a6ebf40388ce4575f4d3e8697
ahmadov-mahammad <dev.ahmadov.mahammad@gmail.com>
2026-06-22 00:59:07 +0400

    problems: add working solution to q15, three sum, using hashset, time complexity On^2 and additional memory space, planning to use Array.sort + current solution with two pointers, time complexity still On^2 +  skippable Onlogn and O1 memory space\

--- problems/0015-three-sum.cs
+++ problems/0015-three-sum.cs
@@ -2,24 +2,67 @@ public class Solution
 {
     public IList<IList<int>> ThreeSum(int[] nums)
     {
+        // Solution 1 : time limit exceeded, Big O(n^3)
+
+        // IList<IList<int>> ans = new List<IList<int>>();
+
+        // Array.Sort(nums);
+
+        // for(int i = 0; i < nums.Length; i++)
+        // {
+        //     for(int j = i + 1; j < nums.Length; j++)
+        //     {
+        //         for(int k = j + 1; k < nums.Length; k++)
+        //         {
+        //             if(nums[i] + nums[j] + nums[k] == 0)
+        //             {
+        //                 if (!ans.Any(a => a[0] == nums[i] && a[1] == nums[j] && a[2] == nums[k]))
+        //                 {
+        //                     ans.Add([nums[i], nums[j], nums[k]]);
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+
+        // return ans;
+
+
+        // Solution 2 : 
+        
         IList<IList<int>> ans = new List<IList<int>>();
 
-        Array.Sort(nums);
+        HashSet<int> seen = new HashSet<int>();
 
-        for(int i = 0; i < nums.Length; i++)
+        HashSet<ValueTuple<int, int, int>> uniqueTriplets = new HashSet<ValueTuple<int, int, int>>();
+
+        for (int i = 0; i <= nums.Length - 3; i++)
         {
-            for(int j = i + 1; j < nums.Length; j++)
+            int target = -nums[i];
+
+            seen.Clear();
+
+            for (int j = i + 1; j < nums.Length; j++)
             {
-                for(int k = j + 1; k < nums.Length; k++)
+                int required = target - nums[j];
+
+                if (seen.Contains(required))
                 {
-                    if(i != j && j != k && i != k && nums[i] + nums[j] + nums[k] == 0)
+                    int min = Math.Min(nums[i], Math.Min(required, nums[j]));
+
+                    int max = Math.Max(nums[i], Math.Max(required, nums[j]));
+
+                    int mid = nums[i] + required + nums[j] - min - max;
+
+                    ValueTuple<int, int, int> vt = ValueTuple.Create(min, mid, max);
+
+                    if (uniqueTriplets.Add(vt))
                     {
-                        if (!ans.Any(a => a[0] == nums[i] && a[1] == nums[j] && a[2] == nums[k]))
-                        {
-                            ans.Add([nums[i], nums[j], nums[k]]);
-                        }
+                        ans.Add([nums[i], required, nums[j]]);
                     }
                 }
+
+                seen.Add(nums[j]);
             }
         }
 
